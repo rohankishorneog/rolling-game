660a225469fbb4ab0e283360a12c3d6d230842e8
rohankishorneog <rkn2601@gmail.com>
2024-11-19 17:28:43 +0530

    random block implementation

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useMemo } from "react";
 import BlockStart from "./BlockStart";
 import BlockSpinner from "./BlockSpinner";
 import BlockLimbo from "./BlockLimbo";
 import BlockAxe from "./BlockAxe";
 import BlockEnd from "./BlockEnd";
 
-const Level = () => {
+const Level = ({ count = 5, types = [BlockSpinner, BlockAxe, BlockLimbo] }) => {
+  const blocks = useMemo(() => {
+    const blocks = [];
+    for (let i = 0; i < count; i++) {
+      const type = types[Math.floor(Math.random() * types.length)];
+      blocks.push(type);
+    }
+
+    return blocks;
+  }, [count, types]);
   return (
     <>
-      <BlockStart position={[0, 0, 16]} />
-      <BlockSpinner position={[0, 0, 12]} />
-      <BlockLimbo position={[0, 0, 8]} />
-      <BlockAxe position={[0, 0, 4]} />
-      <BlockEnd position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 0]} />
+      {blocks.map((Block, i) => (
+        <Block key={i} position={[0, 0, -(i + 1) * 4]} />
+      ))}
+      <BlockEnd position={[0, 0, -(count + 1) * 4]} />
     </>
   );
 };

d14bb5b1b9c275167e215c4b03b7910bb5501195
rohankishorneog <rkn2601@gmail.com>
2024-11-19 17:54:06 +0530

    added controls to move the ball on the plane

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,13 +1,53 @@
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
-import React from "react";
+import React, { useRef } from "react";
 
 const Player = () => {
+  const body = useRef();
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 1 * delta;
+    const torqueStrength = 1 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+    }
+
+    if (rightward) {
+      impulse.x += impulseStrength;
+      torque.z -= torqueStrength;
+    }
+
+    if (backward) {
+      impulse.z += impulseStrength;
+      torque.x += torqueStrength;
+    }
+
+    if (leftward) {
+      impulse.x -= impulseStrength;
+      torque.z += torqueStrength;
+    }
+
+    body.current.applyImpulse(impulse);
+    body.current.applyTorqueImpulse(torque);
+  });
   return (
     <RigidBody
       position={[0, 1, 0]}
       restitution={0.2}
       friction={1}
       colliders="ball"
+      ref={body}
+      linearDamping={0.5}
+      angularDamping={0.5}
+      canSleep={false}
     >
       <mesh castShadow>
         <icosahedronGeometry args={[0.3, 1]} />

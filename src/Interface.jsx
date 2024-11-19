ede556e41119bf40e15b786979b7f205bba85436
rohankishorneog <rkn2601@gmail.com>
2024-11-19 21:00:57 +0530

    created the UI for control and time

---
+++ src/Interface.jsx
@@ -0,0 +1,32 @@
+import { useKeyboardControls } from "@react-three/drei";
+import React from "react";
+
+const Interface = () => {
+  const controls = useKeyboardControls((state) => state);
+  console.log(controls);
+  return (
+    <div className="fixed top-0 left-0 w-full h-full  pointer-events-none ">
+      <div className="absolute top-[15%] left-0 w-full  text-lg bg-[#00000033] pt-2 text-center text-[#ffffff] font-semibold">
+        Time 0.0
+      </div>
+      <div className="flex justify-center absolute top-[40%] left-0 w-full text-7xl bg-[#00000033] pointer-events-auto cursor-pointer text-white">
+        RESTART
+      </div>
+      <div className="absolute bottom-[10%] left-0 w-full">
+        <div className="flex justify-center mb-4">
+          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
+        </div>
+        <div className="flex justify-center mb-4">
+          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
+          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
+          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
+        </div>
+        <div className="flex justify-center">
+          <div className="w-36 h-10 m-1 border-2 border-white bg-white/25"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Interface;

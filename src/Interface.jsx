c76fb4236c704ea79094a930b4ce8f094bf96cfc
rohankishorneog <rkn2601@gmail.com>
2024-11-20 16:13:58 +0530

    updated the css of controls

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -2,8 +2,9 @@ import { useKeyboardControls } from "@react-three/drei";
 import React from "react";
 
 const Interface = () => {
-  const controls = useKeyboardControls((state) => state);
-  console.log(controls);
+  const { forward, backward, leftward, rightward, jump } = useKeyboardControls(
+    (state) => state
+  );
   return (
     <div className="fixed top-0 left-0 w-full h-full  pointer-events-none ">
       <div className="absolute top-[15%] left-0 w-full  text-lg bg-[#00000033] pt-2 text-center text-[#ffffff] font-semibold">
@@ -14,15 +15,35 @@ const Interface = () => {
       </div>
       <div className="absolute bottom-[10%] left-0 w-full">
         <div className="flex justify-center mb-4">
-          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
+          <div
+            className={`w-10 h-10 m-1 border-2 border-white bg-white/25 ${
+              forward ? "bg-white/60" : ""
+            }`}
+          ></div>
         </div>
         <div className="flex justify-center mb-4">
-          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
-          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
-          <div className="w-10 h-10 m-1 border-2 border-white bg-white/25"></div>
+          <div
+            className={`w-10 h-10 m-1 border-2 border-white bg-white/25 ${
+              leftward ? "bg-white/60" : ""
+            }`}
+          ></div>
+          <div
+            className={`w-10 h-10 m-1 border-2 border-white bg-white/25 ${
+              backward ? "bg-white/60" : ""
+            }`}
+          ></div>
+          <div
+            className={`w-10 h-10 m-1 border-2 border-white bg-white/25 ${
+              rightward ? "bg-white/60" : ""
+            }`}
+          ></div>
         </div>
         <div className="flex justify-center">
-          <div className="w-36 h-10 m-1 border-2 border-white bg-white/25"></div>
+          <div
+            className={`w-36 h-10 m-1 border-2 border-white bg-white/25 ${
+              jump ? "bg-white/60" : ""
+            }`}
+          ></div>
         </div>
       </div>
     </div>
